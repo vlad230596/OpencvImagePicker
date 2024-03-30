6b9e189703624f688bcccf548d5c20884790a4e6
vlad230596 <vlad230596@mail.ru>
2024-03-30 12:12:33 +0300

    Add example with Dpg nodes.

--- main.py
+++ main.py
@@ -2,18 +2,87 @@ import math
 from dataclasses import dataclass
 from pathlib import Path
 
-# import numpy as np
 import cv2
 import glob
 
-import tkinter as tk
-import tkinter.ttk as ttk
-
-from tkinter import filedialog
+import numpy as np
 
 # import EniPy.colors
 # from EniPy import colors
 # from EniPy import eniUtils
+
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+
+# callback runs when user attempts to connect attributes
+def link_callback(sender, app_data):
+    # app_data -> (link_id1, link_id2)
+    print(f'link')
+    dpg.add_node_link(app_data[0], app_data[1], parent=sender)
+
+# callback runs when user attempts to disconnect attributes
+def delink_callback(sender, app_data):
+    # app_data -> link_id
+    print(f'delink')
+    dpg.delete_item(app_data)
+
+def add_action():
+    print(f'add_action')
+    pass
+with dpg.font_registry():
+    default_font = dpg.add_font("fonts/OpenSans-Regular.ttf", 24)
+
+def right_click_cb(sender, app_data):
+    dpg.configure_item("right_click_menu", show=True)
+
+with dpg.handler_registry():
+    dpg.add_mouse_click_handler(button=dpg.mvMouseButton_Right, callback=right_click_cb)
+    
+with dpg.window(label="Right click window", modal=True, show=False, id="right_click_menu", no_title_bar=True):
+    dpg.add_text("All those beautiful files will be deleted.\nThis operation cannot be undone!")
+    dpg.add_separator()
+    dpg.add_checkbox(label="Don't ask me next time")
+    with dpg.group(horizontal=True):
+        dpg.add_button(label="OK", width=75, callback=lambda: dpg.configure_item("right_click_menu", show=False))
+        dpg.add_button(label="Cancel", width=75,
+                       callback=lambda: dpg.configure_item("right_click_menu", show=False))
+
+with dpg.window(label="Tutorial", width=400, height=400):
+    dpg.bind_font(default_font)
+    print(f'w')
+    with dpg.node_editor(callback=link_callback, delink_callback=delink_callback):
+        with dpg.node(label="Node 1"):
+            print(f'n1')
+            with dpg.node_attribute(label="Node A1"):
+                dpg.add_input_float(label="F1", width=150)
+
+            with dpg.node_attribute(label="Node A2", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_input_float(label="F2", width=150)
+
+        with dpg.node(label="Node 2"):
+            print(f'n1')
+            with dpg.node_attribute(label="Node A3"):
+                dpg.add_input_float(label="F3", width=200)
+
+            with dpg.node_attribute(label="Node A4", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_input_float(label="F4", width=200)
+
+with dpg.window(label="Example Window"):
+    dpg.add_text("Hello, world")
+    with dpg.popup(dpg.last_item()):
+        dpg.add_text("A popup")
+
+    dpg.add_button(label="Add", callback=add_action)
+    dpg.add_input_text(label="string", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+
+dpg.create_viewport(title='Custom Title', width=800, height=600)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
 
 class PipelineBlock:
     def __init__(self, name, enabled=True):
@@ -36,11 +105,25 @@ class GrayBlock(PipelineBlock):
         result = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
         return result
 
+class ScaleBlock(PipelineBlock):
+    def __init__(self):
+        PipelineBlock.__init__(self, 'scale')
+
+    scale : float = 1.0
+    def process(self, input):
+        width = int(input.shape[1] * self.scale)
+        height = int(input.shape[0] * self.scale)
+        dim = (width, height)
+        result = cv2.resize(input, dim, interpolation=cv2.INTER_AREA)
+        return result
+
 class BlurBlock(PipelineBlock):
     def __init__(self):
         PipelineBlock.__init__(self, 'blur')
 
     def process(self, input):
+        # blur
+        # medianblur
         result = cv2.GaussianBlur(input, (5, 5), 0)
         return result
 
@@ -51,6 +134,15 @@ class ThresholdBlock(PipelineBlock):
     def process(self, input):
         ret, threshold = cv2.threshold(input, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         return threshold
+class MorphologyExBlock(PipelineBlock):
+    def __init__(self):
+        PipelineBlock.__init__(self, 'MorphologyEx')
+    def process(self, input):
+        kernel = np.ones((13, 13), np.uint8)
+        result = cv2.morphologyEx(input, cv2.MORPH_CLOSE, kernel)
+        return result
+
+# class BorderBlock()cv2.copyMakeBorder
 class PipelineExecutor:
     def __init__(self):
         self.stages = []
@@ -69,7 +161,9 @@ def process(path):
         original = cv2.imread(imagePath)
         
         executor = PipelineExecutor()
+        executor.add(ScaleBlock())
         executor.add(GrayBlock())
+        executor.add(MorphologyExBlock())
         executor.add(BlurBlock())
         executor.add(ThresholdBlock())
 
